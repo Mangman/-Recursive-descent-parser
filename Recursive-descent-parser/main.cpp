732c7fcde73b7df1b3cb2064764beba8d6e64784
Mangman <biochoker@gmail.com>
2018-01-31 10:19:39 +0300

    Initial Commit

---
+++ Recursive-descent-parser/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Recursive-descent-parser
+//
+//  Created by Stepan on 31/01/2018.
+//  Copyright © 2018 Stepan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
