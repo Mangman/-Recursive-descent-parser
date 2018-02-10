ddb5e3a09b95a871a46b84cb17399f743e543d58
Mangman <biochoker@gmail.com>
2018-02-10 19:00:57 +0300

    C-like solution

--- Recursive-descent-parser/main.cpp
+++ Recursive-descent-parser/main.cpp
@@ -1,15 +1,82 @@
-//
-//  main.cpp
-//  Recursive-descent-parser
-//
-//  Created by Stepan on 31/01/2018.
 //  Copyright © 2018 Stepan. All rights reserved.
-//
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+const char* s = "(5*(5+2)-5)/2";
+int p = 0;
+
+int G();
+
+int getE();
+int getT();
+int getP();
+int getN();
+
+int main() {
+    const int val = G();
+    printf("%s=%d\n", s, val);
     return 0;
+}
+
+int G () {
+    p = 0;
+    const int val = getE();
+    if (s[p] != '\0') {std::cout << "aaa!";};
+    return val;
+    
+}
+
+int getE() {
+    int val = getT();
+    while (s[p] == '+' || s[p] == '-') {
+        const char op = s[p]; p++;
+        int val2 = getT();
+        if (op == '+') {
+            val = val+val2;
+        }
+        else {
+            val = val-val2;
+        }
+    }
+    return val;
+}
+
+int getT() {
+    int val = getP();
+    while (s[p] == '*' || s[p] == '/') {
+        const char op = s[p]; p++;
+        int val2 = getP();
+        if (op == '*') {
+            val = val*val2;
+        }
+        else {
+            val = val/val2;
+        }
+    }
+    return val;
+}
+
+
+int getP() {
+    // TODO: maybe '&' implementation
+    if (s[p] == '(') {
+        p++;
+        int val = getE();
+        p++;
+        return val;
+    }
+    else {
+        return getN();
+    }
+}
+
+int getN() {
+    // TODO: negative numbers
+    // TODO: check old and new val (or smth with 'do while')
+    int val = 0;
+    while (s[p] >= '0' && s[p] <= '9') {
+        val = val*10+s[p]-'0';
+        p++;
+    }
+    return val;
 }
